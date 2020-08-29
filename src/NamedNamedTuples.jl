c23eb29c1ee0a8c9279bf71e8fc6c691389b2b28
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2020-08-28 20:48:58 -0400

    Copy over from GridVAMP, add some basic test

--- src/NamedNamedTuples.jl
+++ src/NamedNamedTuples.jl
@@ -1,5 +1,31 @@
 module NamedNamedTuples
+export @NamedNamedTuple
 
-# Write your package code here.
+import NamedTupleTools
+import ConstructionBase
+
+
+macro NamedNamedTuple(Type)
+    quote
+      struct $(esc(Type)){T}
+          x::T
+      end
+
+      Base.getproperty(nnt::$(esc(Type)), p::Symbol) = getproperty(getfield(nnt, :x), p)
+      Base.propertynames(nnt::$(esc(Type))) = propertynames(getfield(nnt, :x))
+      Base.NamedTuple(nnt::$(esc(Type))) = getfield(nnt, :x)
+      Base.convert(::Type{NamedTuple}, nnt::$(esc(Type))) = getfield(nnt, :x)::NamedTuple
+      Base.convert(::Type{$(esc(Type))}, nnt::NamedTuple) = $(esc(Type))(nnt)
+      Base.merge(nnt::$(esc(Type)), fields::NamedTuple) = convert($(esc(Type)), merge(convert(NamedTuple, nnt), fields))
+      Base.merge(fields::NamedTuple, nnt::$(esc(Type))) = convert($(esc(Type)), merge(fields, convert(NamedTuple, nnt)))
+
+      NamedTupleTools.delete(nnt::$(esc(Type)), args...) = convert($(esc(Type)), NamedTupleTools.delete(convert(NamedTuple, nnt), args...))
+
+      function ConstructionBase.setproperties(nnt::$(esc(Type)), patch::NamedTuple)
+        $(esc(Type))(ConstructionBase.setproperties(getfield(nnt, :x), patch))
+      end
+  end
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,27 @@
-using NamedNamedTuples
+import NamedNamedTuples: @NamedNamedTuple
 using Test
+
+import Setfield: @set
 
 @testset "NamedNamedTuples.jl" begin
     # Write your tests here.
+    @NamedNamedTuple MyType
+
+    mt = MyType((x=3, y="hey"))
+    @test mt.x == 3
+    @test mt.y == "hey"
+    @test_throws ErrorException mt.z
+
+    mt2 = @set mt.x = 7
+    @test mt2.x == 7
+    @test mt2.y == "hey"
+
+    # allow field type to change
+    mt3 = @set mt.x = "seven"
+    @test mt3.x == "seven"
+    @test mt3.y == "hey"
+
+    @test_throws MethodError mt.x = 3
+    @test_throws ErrorException mt.x = (x=1, y="one")
+    @test_throws ErrorException mt.y = "hey"
 end
